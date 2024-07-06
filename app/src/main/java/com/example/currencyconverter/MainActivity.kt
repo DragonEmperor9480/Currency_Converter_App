1ccdcf3abf81383a4c18a1a4401fe02c2c207b0d
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-06 16:59:12 +0530

    design a basic UI

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val getText: EditText = findViewById(R.id.Edttxt)
+        val Rupee: Button = findViewById(R.id.btn1)
+        val Dollar: Button = findViewById(R.id.btn2)
+        val Contxt: TextView = findViewById(R.id.vitxt)
+        Rupee.setOnClickListener {
+            val amount = getText.text.toString().toFloat()
+            val rup = 80*amount
+            Contxt.text = "Rupees : $rup"
+
+        }
+
+        Dollar.setOnClickListener {
+            val amount = getText.text.toString().toFloat()
+            val dol = amount/80
+            Contxt.text = "Dollars : $dol"
+
+        }
+
+
     }
 }
